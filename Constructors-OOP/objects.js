da32bf60e498553e952f526eeef65668b31dbf55
Cody Johnson <codyjohnsontx@gmail.com>
2024-04-01 17:08:14 -0500

    initial commit

---
+++ Constructors-OOP/objects.js
@@ -0,0 +1,67 @@
+// Practice Problems for working with Objects! No better way to learn than to do!
+// Help me carry the boats and the logs!
+//
+//
+// How to Contribute::::
+// 1. Fork this Project.
+// 2. Create a separate branch, add your own problems
+// 3. Open a pull request!!
+// Add as many as you want
+// Solutions are at the bottom (NO PEEKING 🫣)
+//
+//
+
+//
+// Problem 1 from class 30: Create an espresso machine class that makes machines with 4 properties and 3 methods
+// and then create a object using it
+//
+
+//
+// Problem 2: Create a class called Car with a constructor
+// that takes in make, model, and year as parameters. The class
+// should have a method called displayInfo() that prints out the make,
+// model, and year of the car using template literal.
+//
+
+//
+//
+// Solutions
+//
+//
+// Problem 1:
+// class EspressoMachine {
+//     constructor(color, make, model, price) {
+//       this.color = color;
+//       this.make = make;
+//       this.model = model;
+//       this.price = price;
+//     }
+//     turnOn() {
+//       console.log("Good day, I am now on!");
+//     }
+//     steam() {
+//       console.log("Steam is now STEAMINNNN");
+//     }
+//     brew() {
+//       console.log("Good stuff coming your way");
+//     }
+//   }
+//
+//
+// Problem 2:
+// class Car {
+//   constructor(make, model, year) {
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
+//   }
+//   displayInfo() {
+//     console.log(
+//       `The make is: ${this.make}, the model is: ${this.model}, and the year is: ${this.year}`
+//     );
+//   }
+// }
+// const newCar = new Car("Toyota", "Tacoma", 2024);
+//
+//
+// Problem 3:
